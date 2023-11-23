0e1450fe3999e8d72fff3bd55b63fd7774c91d4f
Mateo Congo <aidenperse@gmail.com>
2023-11-22 21:45:33 -0500

    Mateo_CV-terminado

--- perfiles/src/app/tab1/tab1.page.ts
+++ perfiles/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
+import { ScrollDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,12 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  // Nombre
+  nombre!: string;
+
+  // Correo
+  email!: EmailValidator;
+  // Telefono
+  Telefono!: number;
 
   // fecha
   minDate: string;
-  // alerta
-  // alerta
+  // Alerta
   alertButtons = ['Aceptar'];
+  //Barra de progreso
+  public progress = 0;
 
   constructor() {
     // Obtener la fecha actual
@@ -21,8 +31,30 @@ export class Tab1Page {
     const mes = ('0' + (hoy.getMonth() +1)).slice(-2);
     const dia = ('0' + hoy.getDate()).slice(-2)
 
-    this.minDate = `${año}-${mes}-${dia}`
+    this.minDate = `${año}-${mes}-${dia}`;
+
+    setInterval(() => {
+      this.progress += 0.01;
+
+      if (this.progress > 1) {
+        setTimeout(() => {
+          this.progress = 0;
+        }, 1000);
+      }
+    }, );
 
   }
 
+  // Metodo para desplegar
+  onScroll(event: CustomEvent<ScrollDetail>){
+    // Posiciòn actual del desplazamiento
+    const scrollTop = event.detail.scrollTop;
+
+    // Calcular el progreso en función del desplacamiento
+
+    const contentHeight = (event.target as HTMLElement).scrollHeight;
+    const clientHeight = (event.target as HTMLElement).clientHeight;
+
+    this.progress = scrollTop / (contentHeight - clientHeight);
+  }
 }
